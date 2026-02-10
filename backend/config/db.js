662ed9282c27225b84d2dd241e7501eb05316f84
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-02-10 15:03:56 +0530

    changed backend url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_URI, {});
+    isConnected = true;
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connection mongoDB:", error);
