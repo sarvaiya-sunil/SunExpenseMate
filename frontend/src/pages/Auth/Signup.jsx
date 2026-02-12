6af868f40b39a9afd0ecbb26dc3dd3613423cc13
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-02-12 15:21:55 +0530

    make it more responsive

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -77,50 +77,54 @@ const Signup = () => {
   };
   return (
     <AuthLayout>
-      <div className="lg:w-100% h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
-        <p className="text-xs text-slate-700 mt-[5px] mb-6">
-          join us today by entering your details below.
-        </p>
-        <form onSubmit={handleSignup}>
-          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
-
-          <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1 gap-4">
-            <Input
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              label="Full Name"
-              placeholder="Sunil Sarvaiya"
-              type="text"
-            />
-            <Input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email"
-              placeholder="sunil@example.com"
-              type="text"
-            />
-            <div className="col-span-2">
-              <Input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                label="Password"
-                placeholder="min. 8 characters"
-                type="password"
-              />
-            </div>
-          </div>
-          {error && <p className="text-xs text-red-500 pb-2.5">{error}</p>}
-          <button type="submit" className="btn-primary">
-            {loading ? "Creating Account..." : "Sign Up"}
-          </button>
-          <p className="text-[13px] text-slate-800 mt-3">
-            Already have an Account?{" "}
-            <Link to="/login" className="font-medium text-primary underline">
-              Login
-            </Link>
+      <div className="w-full h-full flex justify-center px-2 sm:px-4 py-1 sm:py-1">
+        <div className="w-full mx-auto mt-5 sm:mt-3 mb-2">
+          <h3 className="text-xl font-semibold text-black mt-15">
+            Create an Account
+          </h3>
+          <p className="text-xs text-slate-700 mt-[5px] mb-6">
+            join us today by entering your details below.
           </p>
-        </form>
+          <form onSubmit={handleSignup}>
+            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+              <Input
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                label="Full Name"
+                placeholder="Sunil Sarvaiya"
+                type="text"
+              />
+              <Input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label="Email"
+                placeholder="sunil@example.com"
+                type="text"
+              />
+              <div className="col-span-1 md:col-span-2">
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  label="Password"
+                  placeholder="min. 8 characters"
+                  type="password"
+                />
+              </div>
+            </div>
+            {error && <p className="text-xs text-red-500 pb-2.5">{error}</p>}
+            <button type="submit" className="btn-primary">
+              {loading ? "Creating Account..." : "Sign Up"}
+            </button>
+            <p className="text-[13px] text-slate-800 mt-3">
+              Already have an Account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </AuthLayout>
   );
