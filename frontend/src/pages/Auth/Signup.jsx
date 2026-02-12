5fb237f59a6ce7f21983353ca759dbb7569e710a
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-02-12 15:33:11 +0530

    make it more responsive

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -78,7 +78,9 @@ const Signup = () => {
   return (
     <AuthLayout>
       <div className="lg:w-100% h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <h3 className="text-xl font-semibold text-black sm:mt-15">
+          Create an Account
+        </h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           join us today by entering your details below.
         </p>
