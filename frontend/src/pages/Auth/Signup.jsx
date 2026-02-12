b4a943637a5f48ba8f10d9be03f168ca54637972
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-02-12 15:45:55 +0530

    make it more responsive

--- frontend/src/pages/Auth/Signup.jsx
+++ frontend/src/pages/Auth/Signup.jsx
@@ -87,7 +87,7 @@ const Signup = () => {
         <form onSubmit={handleSignup}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:flex sm:flex-col">
             <Input
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
